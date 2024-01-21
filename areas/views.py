61414474d88309d21c1e9440a8ac6dc720ea9c0b
Izabela Markiewicz <102699201+Izabela-Markiewicz@users.noreply.github.com>
2024-01-21 23:36:45 +0000

    Sector Highlight on Hover, Info Side Panel

--- areas/views.py
+++ areas/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Requests to dispaly html pages
 
 
 # Map Page
 def show_map(request):
-    return render(request, 'map.html') # Iteration 1
+    # Iteration 3:
+
+    # Calculation Testing:
+    CrimeAvg = (5+3) / 2
+
+    # Define your variable here
+    my_variable = int(CrimeAvg)
+
+    print(my_variable)  # Check in the console
+
+    # Pass the variable to the template context
+    context = {'my_variable': my_variable}
+    return render(request, 'map.html', context) # Iteration 1
+
+
+def show_crime(request):
+ # Get the value of 'policeName' from the session if it was stored there
+    policeNameTest = request.session.get('clicked_police_name', '')
+
+    context = {'policeNameTest': policeNameTest}
+    return render(request, 'map.html', context)
 
 # Landing Page
 def landing_page(request):
